85b96b5fd4536d97ee967ebd11d7352b4f5d8823
AdityaK2905 <60373662+AdityaK2905@users.noreply.github.com>
2023-01-23 02:00:46 -0600

    basic stuff

--- app/src/main/java/com/example/scoutingapp2023/MainActivity.java
+++ app/src/main/java/com/example/scoutingapp2023/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String scouter;
+    String teamNum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/scoutingapp2023/TeleOpActivity.java
+++ app/src/main/java/com/example/scoutingapp2023/TeleOpActivity.java
@@ -1,4 +1,68 @@
 package com.example.scoutingapp2023;
 
-public class TeleOpActivity {
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TeleOpActivity extends AppCompatActivity {
+
+    String scouter;
+    String teamNum;
+
+    //Cubes
+    int topCubes = 0;
+    int midCubes = 0;
+    int lowCubes = 0;
+    //Cones
+    int topCones = 0;
+    int midCones = 0;
+    int lowCones = 0;
+    //Others
+    int autoScore;
+    int teleOpScore = 0;
+    int totalScore = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teleop_screen);
+    }
+
+
+
+
+
+
+
+
+
+
+    //Increment Methods
+    public void cubeTopPlus(View v){
+        topCubes++;
+    }
+
+    public void cubeMidPlus(View v){
+        midCubes++;
+    }
+
+    public void cubeLowPlus(View v){
+        lowCubes++;
+    }
+
+    public void coneTopPlus(View v){
+        topCones++;
+    }
+
+    public void coneMidPlus(View v){
+        midCones++;
+    }
+
+    public void coneLowPlus(View v){
+        lowCones++;
+    }
+
+
+
 }
